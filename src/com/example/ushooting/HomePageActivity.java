121b34d9f63ca5522545dee23b8d151d972fb43a
zhangshunzeus <zhangshunzeus@163.com>
2016-04-06 15:34:16 +0800

    ...

--- src/com/example/ushooting/HomePageActivity.java
+++ src/com/example/ushooting/HomePageActivity.java
@@ -2,6 +2,8 @@ package com.example.ushooting;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HomePageActivity extends Activity {
 
@@ -10,5 +12,5 @@ public class HomePageActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_page);
 	}
-
+ 
 }
