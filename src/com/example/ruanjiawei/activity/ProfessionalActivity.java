23a10c829d2789e1d0fe152a20229cd9e5bd5bbf
rjw2583886 <297216624@qq.com>
2016-04-08 01:56:32 +0800

    器材 业余摄影  专业摄影 自拍 评论 页面

---
+++ src/com/example/ruanjiawei/activity/ProfessionalActivity.java
@@ -0,0 +1,16 @@
+package com.example.ruanjiawei.activity;
+
+import com.example.ushooting.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class ProfessionalActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.professional);
+	}
+}
---
+++ src/com/example/ruanjiawei/adapter/CommentsBaseAdapter.java
@@ -0,0 +1,33 @@
+package com.example.ruanjiawei.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class CommentsBaseAdapter extends BaseAdapter {
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
