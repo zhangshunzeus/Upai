732d6e416ebbb81aaad708584c6a162b42f67dac
rjw2583886 <297216624@qq.com>
2016-04-07 17:15:07 +0800

    入口 改为了 首页

--- src/com/example/ruanjiawei/activity/ImageDetailsActivity.java
+++ src/com/example/ruanjiawei/activity/ImageDetailsActivity.java
@@ -4,13 +4,16 @@ import com.example.ushooting.R;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class ImageDetailsActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.imagedetails);
+		
 	}
-
+	
 }
