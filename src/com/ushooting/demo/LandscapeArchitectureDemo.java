a9b6b8e7f2d8022570771e94172374b5a9f32379
zhangshunzeus <zhangshunzeus@163.com>
2016-04-08 15:01:17 +0800

    风景建筑 动物萌宠 页面搭建

---
+++ src/com/ushooting/demo/LandscapeArchitectureDemo.java
@@ -0,0 +1,35 @@
+package com.ushooting.demo;
+
+public class LandscapeArchitectureDemo {
+
+	int iv_bg;
+	int iv_bg_transparent;
+	String tv_title;
+	String tv_content;
+	
+	public int getIv_bg() {
+		return iv_bg;
+	}
+	public void setIv_bg(int iv_bg) {
+		this.iv_bg = iv_bg;
+	}
+	public int getIv_bg_transparent() {
+		return iv_bg_transparent;
+	}
+	public void setIv_bg_transparent(int iv_bg_transparent) {
+		this.iv_bg_transparent = iv_bg_transparent;
+	}
+	public String getTv_title() {
+		return tv_title;
+	}
+	public void setTv_title(String tv_title) {
+		this.tv_title = tv_title;
+	}
+	public String getTv_content() {
+		return tv_content;
+	}
+	public void setTv_content(String tv_content) {
+		this.tv_content = tv_content;
+	}
+	
+}
