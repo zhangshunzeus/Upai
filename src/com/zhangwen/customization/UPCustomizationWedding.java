09360bc50c08d8bee8e0a786d3b58d838db91910
zhanggithu <756412858@qq.com>
2016-04-13 16:59:24 +0800

    all the pager

--- src/com/zhangwen/customization/UPCustomizationWedding.java
+++ src/com/zhangwen/customization/UPCustomizationWedding.java
@@ -31,7 +31,6 @@ public class UPCustomizationWedding extends Activity {
 	ImageView returns;
 	ImageView shared;
 
-
 	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
